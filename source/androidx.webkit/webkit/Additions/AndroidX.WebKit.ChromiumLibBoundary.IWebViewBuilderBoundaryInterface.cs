e9e6f2a47bec8782fea3a7c8d984baec204ab634
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-06-19 15:12:42 -0500

    Update AndroidX.WebKit.ChromiumLibBoundary.IWebViewBuilderBoundaryInterface.cs

--- source/androidx.webkit/webkit/Additions/AndroidX.WebKit.ChromiumLibBoundary.IWebViewBuilderBoundaryInterface.cs
+++ source/androidx.webkit/webkit/Additions/AndroidX.WebKit.ChromiumLibBoundary.IWebViewBuilderBoundaryInterface.cs
@@ -2,8 +2,6 @@ namespace AndroidX.WebKit.ChromiumLibBoundary;
 
 public partial class WebViewBuilderBoundaryInterfaceConfig
 {
-    public virtual unsafe void Accept (Java.Lang.Object? chromiumConfig)
-    {
+    public virtual void Accept (Java.Lang.Object? chromiumConfig) =>
         this.Accept ((Java.Util.Functions.IBiConsumer?) chromiumConfig);
-    }
 }
