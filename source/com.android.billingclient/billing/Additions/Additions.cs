475b91a8468d9b0ba43629cc974c3f5db3ec0d40
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-07-16 08:12:51 -0500

    Revert "Fix QueryProductDetailsResult partial class and interface implementation"
    This reverts commit d1de5f6abd286e02c6581b1264ad914b7ff15a40.

--- source/com.android.billingclient/billing/Additions/Additions.cs
+++ source/com.android.billingclient/billing/Additions/Additions.cs
@@ -29,19 +29,9 @@ namespace Android.BillingClient.Api
 
     public partial class QueryProductDetailsResult
     {
-        private BillingResult _result;
-        
-        public BillingResult Result 
-        { 
-            get => _result;
-            set => _result = value;
-        }
+        public BillingResult Result { get; set; }
 
-        public IList<ProductDetails> ProductDetails 
-        { 
-            get => ProductDetailsList;
-            set => throw new NotSupportedException("ProductDetails property is read-only. Use ProductDetailsList instead.");
-        }
+        public IList<ProductDetails> ProductDetails { get; set; }
     }
 
     public class QueryPurchasesResult
@@ -134,12 +124,11 @@ namespace Android.BillingClient.Api
 
             var listener = new InternalProductDetailsResponseListener
             {
-                ProductDetailsResponseHandler = (r, s) => 
+                ProductDetailsResponseHandler = (r, s) => tcs.TrySetResult(new QueryProductDetailsResult
                 {
-                    if (s != null)
-                        s.Result = r;
-                    tcs.TrySetResult(s);
-                }
+                    Result = r,
+                    ProductDetails = s
+                })
             };
 
             QueryProductDetails(productDetailsParams, listener);
@@ -260,10 +249,10 @@ namespace Android.BillingClient.Api
 
     internal class InternalProductDetailsResponseListener : Java.Lang.Object, IProductDetailsResponseListener
     {
-        public Action<BillingResult, QueryProductDetailsResult> ProductDetailsResponseHandler { get; set; }
+        public Action<BillingResult, IList<ProductDetails>> ProductDetailsResponseHandler { get; set; }
 
-        public void OnProductDetailsResponse(BillingResult result, QueryProductDetailsResult queryProductDetailsResult)
-            => ProductDetailsResponseHandler?.Invoke(result, queryProductDetailsResult);
+        public void OnProductDetailsResponse(BillingResult result, IList<ProductDetails> productDetails)
+            => ProductDetailsResponseHandler?.Invoke(result, productDetails);
     }
 
     internal class InternalPurchasesResponseListener : Java.Lang.Object, IPurchasesResponseListener
