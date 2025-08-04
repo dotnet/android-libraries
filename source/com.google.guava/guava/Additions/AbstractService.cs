ecfc16317e8218320c4ac8e9f7e4deaa007b6c5a
Sebastian Seidel <s.seidel@cayas.de>
2025-08-04 22:46:43 +0200

    Basic types exposed for binding

---
+++ source/com.google.guava/guava/Additions/AbstractService.cs
@@ -0,0 +1,10 @@
+namespace Com.Google.Common.Util.Concurrent;
+
+public abstract partial class AbstractService
+{
+    public IService.State? InvokeState()
+    {
+        //TODO Implement
+        throw new NotImplementedException();
+    }
+}
---
+++ source/com.google.guava/guava/Additions/UnsignedInteger.cs
@@ -0,0 +1,11 @@
+namespace Com.Google.Common.Primitives;
+
+public sealed partial class UnsignedInteger
+{
+    public int CompareTo(Java.Lang.Object o)
+    {
+        //TODO Implement
+        
+        throw new NotImplementedException();
+    }
+}
