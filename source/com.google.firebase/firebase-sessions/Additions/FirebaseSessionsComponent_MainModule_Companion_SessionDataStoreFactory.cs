43b6142f633648e8497565064e7601594cc7e1e9
sheiksyedm <sheiksyedm@syncfusion.com>
2025-08-19 19:51:15 +0530

    Compilation issue resolved.

--- source/com.google.firebase/firebase-sessions/Additions/FirebaseSessionsComponent_MainModule_Companion_SessionDataStoreFactory.cs
+++ source/com.google.firebase/firebase-sessions/Additions/FirebaseSessionsComponent_MainModule_Companion_SessionDataStoreFactory.cs
@@ -1,9 +1,16 @@
 namespace Firebase.Sessions;
 
-public partial class FirebaseSessionsComponent_MainModule_Companion_SessionDataStoreFactory
-{
-    Java.Lang.Object? JavaX.Inject.IProvider.Get()
-    {
-        return this.Get();
-    }
-}
+using System;
+
+// This partial class extension is no longer needed as the base class is properly generated
+// and already implements JavaX.Inject.IProvider. The binding generator will create the 
+// proper Java class that extends from Java.Lang.Object and implements the IProvider interface.
+// All required methods including Get() are handled by the base generated class.
+
+// public partial class FirebaseSessionsComponent_MainModule_Companion_SessionDataStoreFactory
+// {
+//     Java.Lang.Object? JavaX.Inject.IProvider.Get()
+//     {
+//         return this.Get();
+//     }
+// }
