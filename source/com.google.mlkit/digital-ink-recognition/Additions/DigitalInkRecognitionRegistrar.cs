0d5b32d46e04ac38d98b283abb6d512c793ee3ed
moljac <mcvjetko@holisticware.net>
2025-09-05 16:39:46 +0200

    comment added

--- source/com.google.mlkit/digital-ink-recognition/Additions/DigitalInkRecognitionRegistrar.cs
+++ source/com.google.mlkit/digital-ink-recognition/Additions/DigitalInkRecognitionRegistrar.cs
@@ -1,10 +1,21 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Google.MLKit.Vision.Digitalink.Recognition.Internal
 {
     public partial class DigitalInkRecognitionRegistrar
     {
+        /*
+            * This is a workaround for
+
+            <attr
+                path="/api/package[@name='com.google.mlkit.vision.digitalink.recognition.internal']/class[@name='DigitalInkRecognitionRegistrar']/method[@name='getComponents' and count(parameter)=0]"
+                name="manageReturn"
+                >
+                System.Collections.Generic.IList &lt; Firebase.Components.Component &gt;
+            </attr>
+        */
         global::System.Collections.Generic.IList<global::Firebase.Components.Component>? global::Firebase.Components.IComponentRegistrar.Components
         {
             get
