c38ed9ed22a9fe88e2ffed2ffa0009e6658eb43b
moljac <mcvjetko@holisticware.net>
2025-10-06 13:28:05 +0200

    initial commit

---
+++ source/org.chromium.net/cronet-shared/Additions/Xamarin.Chromium.CroNet.HttpFlags.FlagValue.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Xamarin.Chromium.CroNet.HttpFlags
+{
+	// Metadata.xml XPath class reference: path="/api/package[@name='org.chromium.net.httpflags']/class[@name='FlagValue']"
+	// [global::Android.Runtime.Register ("org/chromium/net/httpflags/FlagValue", DoNotGenerateAcw=true)]
+	public sealed partial class FlagValue
+	{
+	}
+}
