cf5530c5ba56603f460b9b6a1a49e97ebcbb5d1a
sheiksyedm <sheiksyedm@syncfusion.com>
2025-11-26 12:06:09 +0530

    Fix DataStore serializer type casting issues
    Add explicit type casts to Preferences when calling WriteTo methods. The interface
    implementations now properly cast Java.Lang.Object? to Preferences? before delegating
    to the strongly-typed methods.
    
    Changes:
    - datastore-preferences-core-android: Cast t parameter in both PreferencesFileSerializer and PreferencesSerializer
    - datastore-preferences-core-jvm: Cast t parameter in PreferencesSerializer
    - Removed incorrect PreferencesFileSerializer from JVM variant (class doesn't exist in JVM)
    
    Fixes CI build errors:
    - CS1503: Argument 1: cannot convert from 'Java.Lang.Object' to 'Preferences'
    - CS0540: containing type does not implement interface 'ISerializer' (JVM PreferencesFileSerializer)
    - CS0535/CS0738: interface implementation errors

--- source/androidx.datastore/datastore-preferences-core-android/Additions/PreferencesSerializer.cs
+++ source/androidx.datastore/datastore-preferences-core-android/Additions/PreferencesSerializer.cs
@@ -6,6 +6,6 @@ public partial class PreferencesSerializer
 
     global::Java.Lang.Object? global::AndroidX.DataStore.Core.OkIO.IOkioSerializer.WriteTo(global::Java.Lang.Object? t, global::Square.OkIO.IBufferedSink sink, global::Kotlin.Coroutines.IContinuation p2)
     {
-        return WriteTo(t, sink, p2);
+        return WriteTo((global::AndroidX.DataStore.Preferences.Core.Preferences?)t, sink, p2);
     }
 }
