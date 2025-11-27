4aee1e5667e89e9c349c5164573cdc355ada553b
sheiksyedm <sheiksyedm@syncfusion.com>
2025-11-27 17:01:20 +0530

    Fix CS0433 by renaming JVM DataStore namespace
    The JVM and Android DataStore variants have identical Java package names
    (androidx.datastore.preferences.core) which caused CS0433 type conflicts in
    firebase-common and 50+ other packages.
    
    Solution: Rename JVM variant's C# namespace to avoid collision
    - Android: AndroidX.DataStore.Preferences.Core (unchanged)
    - JVM: AndroidX.DataStore.Preferences.Core.Jvm (renamed via metadata)
    
    Both packages now ship without conflicts. The namespace rename is transparent
    to most consumers as they typically reference the Android variant.
    
    Changes:
    - Metadata.xml: Add managedName="AndroidX.DataStore.Preferences.Core.Jvm"
    - Update Additions files to use new namespace
    - Revert dependencyOnly flag (both packages must ship)
    
    Fixes CS0433 in firebase-common and all dependent packages.
    
    ## Why Previous Attempts Failed
    - dependencyOnly: true caused NU1102 errors (51 packages need JVM variant)
    - extern alias: Can't modify generated code to add extern declarations
    - Removing classes: Broke serializer implementations
    
    Namespace rename is the only viable solution.

--- source/androidx.datastore/datastore-preferences-core-jvm/Additions/PreferencesSerializer.cs
+++ source/androidx.datastore/datastore-preferences-core-jvm/Additions/PreferencesSerializer.cs
@@ -1,4 +1,4 @@
-namespace AndroidX.DataStore.Preferences.Core;
+namespace AndroidX.DataStore.Preferences.Core.Jvm;
 
 public partial class PreferencesSerializer
 {
@@ -6,6 +6,6 @@ public partial class PreferencesSerializer
 
     global::Java.Lang.Object? global::AndroidX.DataStore.Core.OkIO.IOkioSerializer.WriteTo(global::Java.Lang.Object? t, global::Square.OkIO.IBufferedSink sink, global::Kotlin.Coroutines.IContinuation p2)
     {
-        return WriteTo((global::AndroidX.DataStore.Preferences.Core.Preferences?)t, sink, p2);
+        return WriteTo((global::AndroidX.DataStore.Preferences.Core.Jvm.Preferences?)t, sink, p2);
     }
 }
