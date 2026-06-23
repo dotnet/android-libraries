06210c5dc23b28f4ec544f8eaf38b5c28a593fd8
sheiksyedm <sheiksyedm@syncfusion.com>
2026-06-23 20:26:46 +0530

    Fix build errors from version updates
    - hsdp: Add Metadata.xml to rename duplicate ErrorEventArgs (argsType)
    - billing: Fix DeveloperBillingType and ChoiceScreenType property/class collisions
    - play-services-games: Remove DataBufferAdditions.cs and BugWorkarounds.cs
      contents (all referenced types removed in v25.0.0)
    - lifecycle-viewmodel: Remove ViewModelLazy TypeForward (removed in 2.11.0)
    - lifecycle-common: Remove LifecycleCoroutineScope TypeForward (removed in 2.11.0)
    - savedstate: Remove ISavedStateRegistryOwner TypeForward (removed in update)

--- source/androidx.savedstate/savedstate/Additions/TypeForwarders.cs
+++ source/androidx.savedstate/savedstate/Additions/TypeForwarders.cs
@@ -1,6 +1,5 @@
 using System.Runtime.CompilerServices;
 
-[assembly:TypeForwardedTo (typeof (AndroidX.SavedState.ISavedStateRegistryOwner))]
 [assembly:TypeForwardedTo (typeof (AndroidX.SavedState.SavedStateRegistry))]
 [assembly:TypeForwardedTo (typeof (AndroidX.SavedState.SavedStateRegistryController))]
 [assembly:TypeForwardedTo (typeof (AndroidX.SavedState.ViewTreeSavedStateRegistryOwner))]
